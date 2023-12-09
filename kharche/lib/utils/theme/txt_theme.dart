e5493ecc9beb251904044876336b0075c87ec631
shadan <shadan.hussain.2004@gmail.com>
2023-12-09 19:57:14 +0530

    Erros

---
+++ kharche/lib/utils/theme/txt_theme.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+class TextTheme{
+ static const Text inter =Text(
+    'Inter',
+    style: TextStyle(
+      color: Color(0xFF292B2D),
+      fontSize: 96,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700,
+      height: 0,
+    ),
+  );
+ static const Text  titleX=Text(
+      'Title X',
+      style: TextStyle(
+        color: Color(0xFF212224),
+        fontSize: 24,
+        fontFamily: 'Inter',
+        fontWeight: FontWeight.w500,
+        height: 0,
+      ));
+ static const Text title1=Text(
+    'Title 1',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text title2=Text(
+    'Title 2',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text title3=Text(
+    'Title 3',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text regular1=Text(
+    'Regular 1',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text regular2=Text(
+    'Regular 2',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text regular3=Text(
+    'Regular 3',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text small=Text(
+    'Small',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+ static const Text tiny=Text(
+    'Tiny',
+    style: TextStyle(
+      color: Color(0xFF212224),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      height: 0,
+    ),
+  );
+}

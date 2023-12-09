b6042e7ab51e30d686088e207494c1265d582655
Peeyush Das <peeyushdas@pop-os.localdomain>
2023-12-09 11:35:45 +0530

    feat: add color theme file

---
+++ kharche/lib/utils/Theme/color_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Clrtheme {
+  static const Color dark_25 = Color.fromRGBO(122, 126, 128, 1);
+  static const Color dark_50 = Color.fromRGBO(70, 74, 77, 1);
+  static const Color dark_75 = Color.fromRGBO(22, 23, 25, 1);
+  static const Color dark_100 = Color.fromRGBO(13, 14, 15, 1);
+
+  static const Color light_20 = Color.fromRGBO(227, 229, 229, 1);
+  static const Color light_40 = Color.fromRGBO(242, 244, 245, 1);
+  static const Color light_60 = Color.fromRGBO(247, 249, 250, 1);
+  static const Color light_80 = Color.fromRGBO(251, 251, 251, 1);
+  static const Color light_100 = Color.fromRGBO(255, 255, 255, 1);
+
+  static const Color violet_20 = Color.fromRGBO(238, 229, 255, 1);
+  static const Color violet_40 = Color.fromRGBO(211, 189, 255, 1);
+  static const Color violet_60 = Color.fromRGBO(177, 138, 255, 1);
+  static const Color violet_80 = Color.fromRGBO(143, 87, 255, 1);
+  static const Color violet_100 = Color.fromRGBO(127, 61, 255, 1);
+
+  static const Color blue_20 = Color.fromRGBO(189, 220, 255, 1);
+  static const Color blue_40 = Color.fromRGBO(252, 252, 252, 1);
+  static const Color blue_60 = Color.fromRGBO(87, 165, 255, 1);
+  static const Color blue_80 = Color.fromRGBO(36, 138, 255, 1);
+  static const Color blue_100 = Color.fromRGBO(0, 119, 255, 1);
+
+  static const Color green_20 = Color.fromRGBO(207, 250, 234, 1);
+  static const Color green_40 = Color.fromRGBO(147, 234, 202, 1);
+  static const Color green_60 = Color.fromRGBO(101, 209, 170, 1);
+  static const Color green_80 = Color.fromRGBO(42, 183, 132, 1);
+  static const Color green_100 = Color.fromRGBO(0, 168, 107, 1);
+
+  static const Color yellow_20 = Color.fromRGBO(252, 238, 212, 1);
+  static const Color yellow_40 = Color.fromRGBO(252, 221, 161, 1);
+  static const Color yellow_60 = Color.fromRGBO(252, 204, 111, 1);
+  static const Color yellow_80 = Color.fromRGBO(252, 187, 60, 1);
+  static const Color yellow_100 = Color.fromRGBO(252, 172, 18, 1);
+
+  static const Color red_20 = Color.fromRGBO(253, 213, 215, 1);
+  static const Color red_40 = Color.fromRGBO(253, 162, 169, 1);
+  static const Color red_60 = Color.fromRGBO(253, 111, 122, 1);
+  static const Color red_80 = Color.fromRGBO(252, 252, 252, 1);
+  static const Color red_100 = Color.fromRGBO(253, 60, 74, 1);
+}

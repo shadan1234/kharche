1f1c15c654aa17cdcc80d7ea7e4c6fabf709c838
shadan <shadan.hussain.2004@gmail.com>
2023-12-10 19:40:13 +0530

    feat:fixing

--- kharche/lib/utils/theme/txt_theme.dart
+++ kharche/lib/utils/theme/txt_theme.dart
@@ -1,102 +1,68 @@
 import 'package:flutter/material.dart';
-class TextTheme{
- static const Text inter =Text(
-    'Inter',
-    style: TextStyle(
-      color: Color(0xFF292B2D),
-      fontSize: 96,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w700,
-      height: 0,
-    ),
+
+class TextTheme {
+  static const TextStyle txtInter96w700 = TextStyle(
+    color: Color(0xFF292B2D),
+    fontSize: 96,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    height: 0,
   );
- static const Text  titleX=Text(
-      'Title X',
-      style: TextStyle(
-        color: Color(0xFF212224),
-        fontSize: 24,
-        fontFamily: 'Inter',
-        fontWeight: FontWeight.w500,
-        height: 0,
-      ));
- static const Text title1=Text(
-    'Title 1',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+
+  static const TextStyle txtInter64w700 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 64,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    height: 0.02,
   );
- static const Text title2=Text(
-    'Title 2',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter32w700 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 32,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    height: 0.04,
   );
- static const Text title3=Text(
-    'Title 3',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter18w600 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
   );
- static const Text regular1=Text(
-    'Regular 1',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter16w500 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
   );
- static const Text regular2=Text(
-    'Regular 2',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter16w600 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 16,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
   );
- static const Text regular3=Text(
-    'Regular 3',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter14w500 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 14,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0.09,
   );
- static const Text small=Text(
-    'Small',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter13w500 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 13,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0.09,
   );
- static const Text tiny=Text(
-    'Tiny',
-    style: TextStyle(
-      color: Color(0xFF212224),
-      fontSize: 24,
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w500,
-      height: 0,
-    ),
+  static const TextStyle txtInter12w500 = TextStyle(
+    color: Color(0xFF161719),
+    fontSize: 12,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0.08,
   );
 }
